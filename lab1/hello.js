9afd4a7e14ee596542745f82294d2234db78d5ae
ANKIT YADAV <ankityadav74609@gmail.com>
2026-07-23 12:04:40 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,22 @@
+console.log("Ankit Yadav")
+
+// write a function to take two number and return
+//their sum
+function sum(a, b){
+     return a+b;
+}
+
+//or
+
+const add=(a,b) =>
+{
+    return a+b;
+};
+
+//or
+
+const add2=(a,b)=>a+b;
+
+console.log (sum(10,10));
+console.log(add(20,10));
+console.log(add2(20,20));
