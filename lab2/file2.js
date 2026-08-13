ab3d451469e3c38635c370e4b8a8073f69bb475b
ANKIT YADAV <ankityadav74609@gmail.com>
2026-08-13 11:50:23 +0530

    file2,

--- lab2/file2.js
+++ lab2/file2.js
@@ -1,16 +1,53 @@
-import {readFile} from "fs/promises";
+// if a function uses await keyword then the fuction must be async  
+import {readFile, appendFile, writeFile } from "fs/promises";
 
-const readData=(filename)=>{
+const readData=async (filename)=>{
+    try{
      const content = await readFile(filename,"utf-8");
      return content;
+} 
+catch(e)
+{
+    console.log(e.message);
+    console.log("file not found");
+}
+finally{
+   console.log("read data finished");
+}
+};
+
+const writeData=async(filename,content)=>{
+    try
+    {
+      await writeFile(filename,content);
+    
+}
+catch(e)
+{
+    console.log(e.message);
+  
 }
 
-const writeData=(filename,content)=>{
-     const content = await writeFile(filename,content);  
-    return "Data return successfully";
-}
+};
 
-const appendData=(filename,content)=>{
-     const content = await appendFile(filename,content);  
-    return "Data appended successfully";
-}
+const appendData=async(filename,content)=>{
+    try{
+     await appendFile(filename,content);  
+    }
+    catch(error){
+        console.log(error.message);
+    }
+    
+};
+
+const deleteFile=async(filename)=>{
+    try{
+        await unlink(filename);
+    }
+    catch(error){
+        console.log(error.message);
+    }
+};
+
+const data=await readData("file1.js");
+ console.log(data);
