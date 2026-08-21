26679732bb657004ca9d8df7e73d27fa69cb6df2
ANKIT YADAV <ankityadav74609@gmail.com>
2026-08-21 14:10:58 +0530

    lab2completed

--- lab2/crud.js
+++ lab2/crud.js
@@ -1,6 +1,80 @@
 
 import readline from  "readline/promises";
 import {stdin,stdout} from "process";
+import {readFile, writeFile}from "fs/promises";
+
+
+const FILE="product.json";
+
+const getCart = async()=>{
+      const data = await readFile(FILE,"utf-8");
+      return JSON.parse(data); 
+};
+
+const saveCart = async (myCart)=>{
+      await writeFile(FILE,JSON.stringify(myCart, null, 2));
+};
+
+
+const addtocart=async (product)=>{
+    const myCart = await getCart();
+    const isFound = myCart.find((item)=>item.id===product.id);
+    if(isFound){
+        isFound.qty += product.qty;
+    }
+    else{
+        myCart.push(product);
+    }
+    await saveCart(myCart);
+    console.log(`product added/updated with id ${product.id} into cart`);
+};
+
+const showcart=async()=>{
+    const data = await getCart();
+    console.table(data);
+
+    let total=0;
+    // for(let i=0;i<data.length;i++)
+    // {
+    //     total=total+data[i].qty*data[i].price;
+    // }
+    // console.log("total price: ",total);
+
+    total=data.reduce((acc,item) => acc + item.qty * item.price,0);
+    console.log("you have to pay",total);
+
+};
+
+const removefromcart = async(pid)=>{
+    const data=await getCart();
+    const count = data.length;
+    const newdata=data.filter((item)=>item.id !=pid);
+    const newcount=newdata.length;
+    if(count==newcount){
+        console.log(`product with id ${pid} not found`);
+    }
+    else
+    {
+        await saveCart(newdata);
+        console.log(`product with id ${pid} deleted successfully`);
+    }
+};
+
+const updatecart = async (pid,value)=>
+{
+    const data=await getCart();
+    const isfound = data.find((item)=> item.id===pid);
+    if(isfound)
+    {
+        isfound.qty+=value;
+        await saveCart(data);
+        console.log("product quantity update successfully");
+
+    }
+    else{
+        console.log("product id not found");
+    }
+}
 
 const main =async()=>{
  let choice;
@@ -15,18 +89,30 @@ console.log("5......checkout");
 choice = await cin.question ("Enter your choice");
 switch(Number(choice)){
     case 1:
-        console.log("show products");
+        await showcart();
         break;
     case 2:
-        console.log("product added");
+        let data = await cin.question("enter id , name , price, qty:");
+         const [id,name,price,qty] = data.split(',').map((item)=>item.trim())
+         const product={id:Number(id),name,price:Number(price),qty: Number(qty)
+
+         };
+         await addtocart(product);
+         break;
+
+    case 3:
+        // console.log("remove product");
+        let pid=await cin.question("enter product id to remove")
+        await removefromcart(Number(pid));
         break;
+   
     case 4:
-        console.log("remove product");
+        // console.log("update product quantity");
+        let pid2=await cin.question("enter product id to update");
+        let value = await cin.question("+1 increase,-1 decrease");
+        await updatecart(Number(pid2),Number(value));
         break;
     case 5:
-        console.log("update product quantity");
-        break;
-    case 6:
         console.log("see you later");
         break;
     default:
