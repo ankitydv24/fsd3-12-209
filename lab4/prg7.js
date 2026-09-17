cbfc963ca178bb4b0839a6c54851bdf811649250
ANKIT YADAV <ankityadav74609@gmail.com>
2026-09-17 12:16:32 +0530

    add user

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,13 +1,29 @@
 import http from "http";
-import { getUsers } from "./users.js";
+import { addUser, getUsers } from "./users.js";
 const server = http.createServer((req, res) => {
   if (req.url === "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getUsers()));
   } else if (req.url === "/api/users" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const user = JSON.parse(body);
+      const userCreated = addUser(user);
+
+      res.end(JSON.stringify({ msg: "user added", userCreated }));
+    });
     res.end(JSON.stringify({ msg: "add user" }));
   } else if (req.url === "/api/users/1" && req.method === "GET") {
+  
+
     res.end(JSON.stringify({ msg: "single user with id 1" }));
-  } else if (req.url === "/api/users/1" && req.method === "PUT") {
+
+
+  } 
+  
+  else if (req.url === "/api/users/1" && req.method === "PUT") {
     res.end(JSON.stringify({ msg: "update user with id 1" }));
   } else if (req.url === "/api/users/1" && req.method === "DELETE") {
     res.end(JSON.stringify({ msg: "remove user with id 1" }));
